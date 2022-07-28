c78e7bd5cea91f4d456ba6340d13efe83718e931
noondaydream <104835917+thenoondaydream@users.noreply.github.com>
2022-07-29 02:30:18 +0900

    Refactoring: socialLogin 서비스별 분리, 반복 코드 함수화

--- controllers/user.js
+++ controllers/user.js
@@ -17,30 +17,43 @@ async function SocialLoginController(req, res) {
   res.status(201).json({ message: 'SocialLogin_Success' });
 }
 
-async function SocialLoginStatusCodeController(req, res) {
+async function googleLoginController(req, res) {
   // console.log(JSON.stringify(req.body));
   const { state, code, redirectUri } = req.body.req;
-
-  console.log('SocialLoginStatusCodeController');
-
   let data = null;
-
   try {
-    data = await usersService.SocialLoginStatusCodeService(
-      state,
-      code,
-      redirectUri
-    );
+    data = await usersService.getUserInfoByGoogle(state, code, redirectUri);
   } catch (err) {
     res.status(err.statusCode || 500).json({ message: err.message });
     return;
   }
-
-  // prisma 는 프로미스 객체이기 때문에 await async를 쓰거나 then을 통해서 값을 가져와야됨
-
   res.status(201).json({ data: data });
 }
-
+async function naverLoginController(req, res) {
+  // console.log(JSON.stringify(req.body));
+  const { state, code } = req.body.req;
+  let data = null;
+  try {
+    data = await usersService.getUserInfoByNaver(state, code);
+  } catch (err) {
+    res.status(err.statusCode || 500).json({ message: err.message });
+    return;
+  }
+  res.status(201).json({ data: data });
+}
+async function kakaoLoginController(req, res) {
+  // console.log(JSON.stringify(req.body));
+  const { state, code } = req.body.req;
+  let data = null;
+  try {
+    data = await usersService.getUserInfoByKakao(state, code);
+    console.log(data, 'dkfajdlkfasj;ladfs');
+  } catch (err) {
+    res.status(err.statusCode || 500).json({ message: err.message });
+    return;
+  }
+  res.status(201).json({ data: data });
+}
 async function watchHistoryList(req, res) {
   try {
     const userId = req.userId;
@@ -87,11 +100,27 @@ async function likeHistoryController(req, res) {
   }
 }
 
+// async function profileController(req, res) {
+//   try {
+//     const userId = req.userId;
+//     const nickname = req.body.nickname;
+
+//     await usersService.updateNickname(userId, nickname);
+//     return res.status(201).json({ message: 'LIKE_HISTORY_DELETED' });
+//   } catch (err) {
+//     console.log(err);
+//     return res.status(err.statusCode || 500).json({ message: err.message });
+//   }
+// }
+
 module.exports = {
   SocialLoginController,
-  SocialLoginStatusCodeController,
+  googleLoginController,
+  naverLoginController,
+  kakaoLoginController,
   watchHistoryList,
   likeList,
   watchHistoryController,
   likeHistoryController,
+  // profileController,
 };
